184096d8b1a4565fc0e599a768cf40e30759d897
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2025-06-22 09:59:11 -0400

    asjust
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/ctrl_node.tf
+++ terraform/ctrl_node.tf
@@ -38,7 +38,7 @@ resource "proxmox_virtual_environment_vm" "ctrl_rancher_vm" {
   }
 
   memory {
-    dedicated = 28672       # fixed RAM allocation in MiB
+    dedicated = 24576       # fixed RAM allocation in MiB
     hugepages = var.enable_hugepages ? var.hugepages_value : null
   }
 
