0b71be3ca9e3fb41c5317e5646f273bffe1d3250
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2025-07-07 08:41:10 -0400

    update
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/ctrl_node.tf
+++ terraform/ctrl_node.tf
@@ -49,13 +49,13 @@ resource "proxmox_virtual_environment_vm" "ctrl_rancher_vm" {
   numa {
     device     = "numa0"
     cpus       = "0-9"
-    memory     = 65536
+    memory     = 61440
     hostnodes  = "2"
     policy     = "bind"
   }
 
   memory {
-    dedicated = 65536       # fixed RAM allocation in MiB
+    dedicated = 61440       # fixed RAM allocation in MiB
     hugepages = var.enable_hugepages ? var.hugepages_value : null
   }
 
