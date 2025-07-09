e692dbd6e63578f6db04d28c32636defbff048d3
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2025-07-09 13:53:31 -0400

    update
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/vault-sa.tf
+++ terraform/vault-sa.tf
@@ -18,14 +18,14 @@ resource "google_service_account" "vault_unseal" {
 }
 
 resource "google_service_account_key" "vault_key" {
-  service_account_id = google_service_account.vault_unseal.name
+  service_account_id = google_service_account.vault_unseal.email
   private_key_type   = "TYPE_GOOGLE_CREDENTIALS_FILE"
 
   depends_on = [
       google_service_account.vault_unseal,
       google_kms_crypto_key.vault_key,
       google_kms_crypto_key_iam_member.vault_kms_crypto_access,
-      google_project_iam_custom_role.vault_kms_crypto_role
+      google_project_iam_custom_role.vault_kms_crypto_ops_role
   ]
 }
 
@@ -35,10 +35,13 @@ locals {
 
 resource "google_kms_crypto_key_iam_member" "vault_kms_crypto_access" {
   crypto_key_id = google_kms_crypto_key.vault_key.id
-  role          = "projects/${google_project.infra.project_id}/roles/${google_project_iam_custom_role.vault_kms_crypto_role.role_id}"
+  role          = "projects/${google_project.infra.project_id}/roles/${google_project_iam_custom_role.vault_kms_crypto_ops_role.role_id}"
   member        = "serviceAccount:${google_service_account.vault_unseal.email}"
 
-  depends_on = [google_project_iam_custom_role.vault_kms_crypto_role]
+  depends_on = [
+        google_project_iam_custom_role.vault_kms_crypto_ops_role,
+        null_resource.wait_for_custom_role
+  ]
 }
 
 # give GCP’s KMS service-agent permission to view key usage
@@ -48,8 +51,16 @@ resource "google_organization_iam_member" "kms_org_service_agent" {
   member = "serviceAccount:service-org-${var.gcp_org_id}@gcp-sa-cloudkms.iam.gserviceaccount.com"
 }
 
-resource "google_project_iam_custom_role" "vault_kms_crypto_role" {
-  role_id     = "vaultKmsCryptoAccessRole"
+resource "null_resource" "wait_for_custom_role" {
+  depends_on = [google_project_iam_custom_role.vault_kms_crypto_ops_role]
+
+  provisioner "local-exec" {
+    command = "echo 'Waiting for IAM role to propagate...'; sleep 60"
+  }
+}
+
+resource "google_project_iam_custom_role" "vault_kms_crypto_ops_role" {
+  role_id     = "vaultKmsCryptoAccessCustomRole"
   title       = "Vault KMS Crypto Access"
   description = "Minimal permissions to allow Vault auto-unseal via GCP KMS"
   project     = google_project.infra.project_id
