e9226352d6cbd181fadc150117c76c140b46f2b6
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2025-07-09 08:12:52 -0400

    update
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/eventarc.tf
+++ terraform/eventarc.tf
@@ -28,7 +28,8 @@ resource "google_eventarc_trigger" "secret_manager_trigger" {
   service_account = google_service_account.eventarc_service_account.email
 
   depends_on = [
-    google_project_service.project_service
+    google_project_service.project_service,
+    google_cloud_run_v2_service.vault_sync_svc
   ]
 }
 
--- terraform/vault_sync_svc.tf
+++ terraform/vault_sync_svc.tf
@@ -88,8 +88,7 @@ resource "google_cloud_run_v2_service" "vault_sync_svc" {
     google_service_account.eventarc_service_account,
     google_project_iam_member.cloud_run_secret_access,
     google_pubsub_topic.secret_manager_events,
-    google_project_iam_member.eventarc_receive_auditlog,
-    google_eventarc_trigger.secret_manager_trigger
+    google_project_iam_member.eventarc_receive_auditlog
   ]
 
 }
@@ -101,7 +100,10 @@ resource "google_cloud_run_service_iam_policy" "noauth-user-profile" {
 
   policy_data = data.google_iam_policy.noauth.policy_data
 
-  depends_on = [ google_cloud_run_v2_service.vault_sync_svc ]
+  depends_on = [ 
+      google_cloud_run_v2_service.vault_sync_svc,
+      google_eventarc_trigger.secret_manager_trigger
+   ]
 }
 
 resource "google_artifact_registry_repository" "vault_sync_repo" {
