90af91704724ffd538d053fea90087d339f532a7
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2025-07-11 18:00:00 -0400

    update
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/vault_sync_svc.tf
+++ terraform/vault_sync_svc.tf
@@ -179,6 +179,6 @@ resource "null_resource" "ghcr_to_gcp_image_sync" {
 
   depends_on = [
     google_artifact_registry_repository.vault_sync_repo,
-    null_resource.fetch_ghcr_tag
+    null_resource.get_ghcr_tag
   ]
 }
