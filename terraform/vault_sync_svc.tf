b291b61fa8f14d76b03a71fac04cc78e660e1a76
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2025-07-23 20:25:23 -0400

    adding env to ignore
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/vault_sync_svc.tf
+++ terraform/vault_sync_svc.tf
@@ -82,7 +82,10 @@ resource "google_cloud_run_v2_service" "vault_sync_svc" {
   }
 
   lifecycle {
-    ignore_changes = [template[0].containers[0].image]
+    ignore_changes = [
+      template[0].containers[0].image,
+      template[0].containers[0].env
+    ]
   }
 
   depends_on = [
