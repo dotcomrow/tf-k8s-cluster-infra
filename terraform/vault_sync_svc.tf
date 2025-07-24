84a1374f67e200cf87593b505aafa2d4e0170454
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2025-07-24 09:05:52 -0400

    update
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/vault_sync_svc.tf
+++ terraform/vault_sync_svc.tf
@@ -84,7 +84,9 @@ resource "google_cloud_run_v2_service" "vault_sync_svc" {
   lifecycle {
     ignore_changes = [
       template[0].containers[0].image,
-      template[0].containers[0].env
+      template[0].containers[0].env,
+      client,
+      client_version,
     ]
   }
 
