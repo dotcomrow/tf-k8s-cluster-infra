a0a047c640dd671796ab09a3e2311dad17f3e972
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2026-01-03 15:14:42 -0500

    adjusting sizes to try to reach 14 days of metrics and logs and traces.
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/work_node_t4_gpu.tf
+++ terraform/work_node_t4_gpu.tf
@@ -145,7 +145,7 @@ resource "proxmox_virtual_environment_vm" "work_t4_gpu_rancher_vm" {
     interface    = "scsi2"
     iothread     = true
     discard      = "on"
-    size         = 650
+    size         = 700
     file_format  = "raw"
     cache        = "unsafe"
   }
