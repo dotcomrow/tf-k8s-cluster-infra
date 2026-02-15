07fcec523b2eaf04a98d146dab751232f1dc802b
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2026-02-15 10:20:19 -0500

    updating teleport bot image
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/vault_sync_svc.tf
+++ terraform/vault_sync_svc.tf
@@ -77,8 +77,8 @@ locals {
 # --- DRY helpers ---
 locals {
   app_image  = "${var.region}-docker.pkg.dev/${google_project.infra.project_id}/vault-sync-run-container/vault-sync-run-container:${local.image_tag}"
-  tbot_image_digest = "sha256:609882855326e04e0c1feea1d687622f58966b16ff60a859b403923984b7a0e3"
-  tbot_ghcr_image_tag = "ts-20260208-215740"
+  tbot_image_digest = "sha256:a3e8181b40188685e884bc5bc8bf91bd943dc9d99b00eb2d68ad0d4ba6d37a5e"
+  tbot_ghcr_image_tag = "ts-20260215-151751"
   tbot_image_name   = "teleport-bot-container"
   tbot_source_image = "ghcr.io/${var.GITHUB_ORG}/${local.tbot_image_name}:${local.tbot_ghcr_image_tag}"
   tbot_target_repo  = "${var.region}-docker.pkg.dev/${google_project.infra.project_id}/thirdparty/${local.tbot_image_name}"
