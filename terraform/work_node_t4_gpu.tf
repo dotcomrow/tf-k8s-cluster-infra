a13c0f904a30a87d09661df3637890ae63968751
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2026-04-29 15:42:28 -0400

    updating to revert back to working t4
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/work_node_t4_gpu.tf
+++ terraform/work_node_t4_gpu.tf
@@ -92,7 +92,7 @@ resource "proxmox_virtual_environment_vm" "work_t4_gpu_rancher_vm" {
   # Keep known-good passthrough args for T4 on this host/QEMU combo.
   # Without MSI-X relocation, QEMU can fail with:
   # "MSIX PBA outside of specified BAR".
-  kvm_arguments = "-fw_cfg name=opt/ovmf/X-PciMmio64Mb,string=131072 -set device.hostpci0.x-msix-relocation=bar5"
+  # kvm_arguments = "-fw_cfg name=opt/ovmf/X-PciMmio64Mb,string=131072 -set device.hostpci0.x-msix-relocation=bar5"
 
   efi_disk {
     datastore_id = var.VM_DISK_STORAGE
